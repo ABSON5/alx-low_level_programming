0088e04b3db1f46d95012e6d2f188e6cdcb4d475
ABSO5 <abynaison@gmail.com>
2023-07-17 14:04:34 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * This is now a program that really prints a lin e using the Printf thingy function kkk
- * return:0 (success)
+ *This is now a program that really prints a lie with printf function
+ *return:0 (success)
  */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
