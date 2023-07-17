c4ba3948416eccb494d5b395cb46e749c3edb12e
ABSO5 <abynaison@gmail.com>
2023-07-17 13:15:19 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include < stdio.h>
+/**
+ * main - This program prints a line with usage of the puts function
+ * Return:0 (will be successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
