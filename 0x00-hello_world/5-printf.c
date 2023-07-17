ff0be497992387cc6b37afe590a053a84928d645
ABSO5 <abynaison@gmail.com>
2023-07-17 13:52:35 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * This is now a program that really prints a lin e using the Printf thingy function kkk
+ * return:0 (success)
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
